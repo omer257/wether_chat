b523f3bc9586f75025bbf585eaf75e536990cb0c
Omi <omer257@gmail.com>
2018-05-09 11:16:39 +0300

    Update view.js
    Suggested solution, finish before running

--- public/view.js
+++ public/view.js
@@ -3,10 +3,12 @@ import { $display } from './main.js'
 import { weatherData } from './main.js'
 
 class View {
-    constructor() {}
+    constructor(DataObject) {
+        this.dataObject = DataObject
+    }
     renderView() {
         $display.empty();
-        weatherData.getFromLocalStorage();
+        this.dataObject.getFromLocalStorage();
         for (var i = 0; i < weatherData.weatherResult.length; i++) {
             var template = Handlebars.compile($('#template').html())
             var newHTML = template(weatherData.weatherResult[i]);
